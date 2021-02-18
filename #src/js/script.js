11c111b3151ed2345f2296e05b937de2672499b5
Alexander Snarsky <snarsky93@gmail.com>
2021-02-18 23:22:38 +0300

    feat: add active nav link

--- #src/js/script.js
+++ #src/js/script.js
@@ -1,1 +1,14 @@
-@@include('alert.js')
+@@include('alert.js');
+
+let navContainer = document.querySelector('.navigation');
+let arrayNavItems = Array.from(navContainer.querySelectorAll('.navigation__link'));
+
+function clickHandlerNavigation(event) {
+    let target = event.target;
+
+    arrayNavItems.forEach(el => el.classList.remove('navigation__link-active'));
+    target.classList.add('navigation__link-active');
+
+}
+
+navContainer.addEventListener('click', clickHandlerNavigation);
